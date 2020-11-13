a83e296447e80c3a0f99b8daf7cb071d2905e0a3
Vetle Roos Mangrud <vmangrud@gmail.com>
2020-11-13 15:07:07 +0100

    Added error message on wrong login credentials

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,6 +4,7 @@
     Navn: <input id="name" v-model="name" /><br />
     Passord: <input id="password" v-model="password" type="password" /><br />
     <button v-on:click="sendLogin">Logg inn</button>
+    <p id="errorMesage" v-if="errorMessage">Feil brukernavn eller passord</p>
   </div>
 </template>
 
@@ -15,15 +16,25 @@ export default {
   data() {
     return {
       name: [],
-      password: []
+      password: [],
+      errorMessage: false
     };
   },
   methods: {
     sendLogin: function() {
       axios
         .post("http://localhost:9000/login", { name: this.name, password: this.password})
-        .then(res => console.log(res))
-        .catch(error => console.log(error));
+        .then(res => {
+          console.log(res);
+          this.errorMessage = false;
+          this.$router.push({ name: 'toppliste' });
+        })
+        .catch(error => {
+          console.log(error);
+          if (error.response.status == 400 || error.response.status == 401) {
+            this.errorMessage = true;
+          }
+        });
     }
   }
 };
